cbd17cf1ebe20240e1c6a4bfefd3db346aa59030
Nithin <nithin@konceptslab.com>
2024-11-14 15:55:26 +0530

    14-11-2024

--- src/components/events/events-carousel.tsx
+++ src/components/events/events-carousel.tsx
@@ -57,36 +57,8 @@ const EventsCarousel: React.FC<HomeCarouselProps> = ({ style_2, style_3 }) => {
     <> 
       <section className={`${styles['home-carousel']} ${styles['bg-light-black']}`}>
         <div className="container-fluid">
-          {style_3 ? (
-            <div className="cs_section_heading cs_style_1 cs_type_1">
-              <div className="cs_section_heading_text">
-                <div className="cs_section_subtitle anim_div_ShowZoom">
-                  Our Blog
-                </div>
-                <h2 className="cs_section_title anim_heading_title">
-                  New Day <br /> New Inspiration
-                </h2>
-              </div>
-              <div className="cs_section_heading_right cs_btn_anim">
-                <Link href="/blog" className="cs_btn cs_style_1">
-                  <span>View All Blogs</span>
-                  <svg
-                    width="19"
-                    height="13"
-                    viewBox="0 0 19 13"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M18.5303 7.03033C18.8232 6.73744 18.8232 6.26256 18.5303 5.96967L13.7574 1.1967C13.4645 0.903806 12.9896 0.903806 12.6967 1.1967C12.4038 1.48959 12.4038 1.96447 12.6967 2.25736L16.9393 6.5L12.6967 10.7426C12.4038 11.0355 12.4038 11.5104 12.6967 11.8033C12.9896 12.0962 13.4645 12.0962 13.7574 11.8033L18.5303 7.03033ZM0 7.25H18V5.75H0V7.25Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                </Link>
-              </div>
-            </div>
-          ) : (
-            <div className="cs_section_heading cs_style_1 cs_type_1">
+         
+            <div className="cs_section_heading cs_style_1 cs_type_1 m-same-line">
               <div className="cs_section_heading_text">
             
                 <h3>Recent Events</h3>
@@ -109,7 +81,7 @@ const EventsCarousel: React.FC<HomeCarouselProps> = ({ style_2, style_3 }) => {
                 </Link>
               </div>
             </div>
-          )}
+        
           
           <Swiper
             modules={[Autoplay]}
