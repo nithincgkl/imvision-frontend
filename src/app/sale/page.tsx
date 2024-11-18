01597e5d955018a67a47ebbb5fc98ccd355f5893
Nithin <nithin@konceptslab.com>
2024-11-18 18:30:19 +0530

    18-11-2024

--- src/app/sale/page.tsx
+++ src/app/sale/page.tsx
@@ -1,25 +1,33 @@
-'use client'
+'use client';
 import style from "./style.module.css";
 import React, { useState } from 'react';
-import HeaderOne from '@/layouts/headers/HeaderOne'
-import Wrapper from '@/layouts/wrapper'
-import LetsTalk from '@/components/home/lets-talk'
-import FooterOne from '@/layouts/footers/FooterOne'
-
+import HeaderOne from '@/layouts/headers/HeaderOne';
+import Wrapper from '@/layouts/wrapper';
+import LetsTalk from '@/components/home/lets-talk';
+import FooterOne from '@/layouts/footers/FooterOne';
 
 const Page: React.FC = () => {
+    // State to manage the visibility of the section
+    const [showFilterSection, setShowFilterSection] = useState(false);
+
+    // Toggle the visibility of the section
+    const toggleFilterSection = () => {
+        setShowFilterSection((prev) => !prev);
+    };
+
+    // Close the filter section
+    const closeFilterSection = () => {
+        setShowFilterSection(false);
+    };
+
     return (
-
-
         <Wrapper>
             <HeaderOne />
             <div id="smooth-wrapper">
-                <div id="smooth-content" className='smooth-content'>
+                <div id="smooth-content" className="smooth-content">
                     <main>
                         <section className={style["without-banner"]}>
                             <div className={style["sale_container"]}>
-
-
                                 <div className="container">
                                     <div className="row">
                                         <div className="col-12">
@@ -29,16 +37,19 @@ const Page: React.FC = () => {
                                     </div>
                                 </div>
 
-
                                 <div className={style["sale_filter_container"]}>
                                     <div className="container-fluid">
                                         <div className="row">
                                             <div className="col-6">
-                                                <button className={style.filter_button}>Filter Product List </button>
+                                                <button
+                                                    className={style.filter_button}
+                                                    onClick={toggleFilterSection}
+                                                >
+                                                    Filter Product List
+                                                </button>
                                             </div>
                                             <div className="col-6">
                                                 <div className={style.sale_filter_right}>
-
                                                     <button>Default Sorting</button>
                                                     <p>Showing 1-12 of 92 results</p>
                                                 </div>
@@ -47,12 +58,22 @@ const Page: React.FC = () => {
                                     </div>
                                 </div>
 
-
+                                {/* Conditionally render the section */}
+                                {showFilterSection && (
+                                    <section className={style["sale_filter_inner_container"]}>
+                                        {/* Close button inside the filter section */}
+                                        <button
+                                            className={style.close_button}
+                                            onClick={closeFilterSection}
+                                        >
+                                            &times;
+                                        </button>
+                                        {/* Filter content goes here */}
+                                        <p>Filter section content goes here.</p>
+                                    </section>
+                                )}
                             </div>
                         </section>
-
-
-
 
                         <LetsTalk />
                         <FooterOne />
@@ -60,9 +81,6 @@ const Page: React.FC = () => {
                 </div>
             </div>
         </Wrapper>
-
-
-
     );
 };
 
