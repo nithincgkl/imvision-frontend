b10af691a863f699b09fa85acd70cfa06722f7e6
Nithin <nithin@konceptslab.com>
2024-11-29 10:35:56 +0530

    conflict resolved

--- src/components/sale/filter.tsx
+++ src/components/sale/filter.tsx
@@ -96,15 +96,11 @@ const Filter: React.FC = () => {
         },
       ],
     },
-<<<<<<< HEAD
-=======
-    // ... Other categories
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
   ];
 
   const toggleSelection = (
-    selectedItems: string[], 
-    item: string, 
+    selectedItems: string[],
+    item: string,
     setSelected: React.Dispatch<React.SetStateAction<string[]>>
   ) => {
     setSelected(
@@ -126,8 +122,8 @@ const Filter: React.FC = () => {
 
   const filteredSubSubCategories = categories
     .filter((cat) => selectedCategories.includes(cat.name))
-    .flatMap((cat) => 
-      cat.subCategories.filter((subCat) => 
+    .flatMap((cat) =>
+      cat.subCategories.filter((subCat) =>
         selectedSubCategories.includes(subCat.name)
       )
     )
@@ -135,55 +131,32 @@ const Filter: React.FC = () => {
 
   return (
     <div>
-<<<<<<< HEAD
-=======
       {/* Common Top Section */}
       {/* Event Gallery Section */}
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
       <section className={style.sale_filter_container}>
         <div className="container-fluid">
           <div className="row">
             <div className="col-6">
-<<<<<<< HEAD
-              <button 
-                onClick={() => setShowFilter(!showFilter)} 
-                className={style.filter_btn}
-              >
-                Filter Category
-=======
-            
-              
+
+
               <button onClick={() => setShowFilter(!showFilter)} className={style.filter_btn}>
-                Filter Category button 
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
+                Filter Category button
               </button>
-              <FaAngleDown  className={style.btn_cat} />
+              <FaAngleDown className={style.btn_cat} />
             </div>
             <div className="col-6">
               <div className={style.sale_filter_container_right}>
-<<<<<<< HEAD
-                <p className={style.m_none}>Showing 1-12 of 92 results</p>
-                <select 
-                  className={style.sort_dropdown} 
-                  onChange={(e) => console.log(e.target.value)}
-                >
-                  <option value="">Sort By</option>
-=======
                 <p className={style.m_none}>
-                  Showing 1-12 of 92 results 
+                  Showing 1-12 of 92 results
                 </p>
                 <select className={style.sort_dropdown} onChange={(e) => console.log(e.target.value)}>
                   <option value="">Default Sorting </option>
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
                   <option value="price-low-to-high">Price: Low to High</option>
                   <option value="price-high-to-low">Price: High to Low</option>
                   <option value="newest">Newest</option>
                   <option value="rating">Rating</option>
                 </select>
-<<<<<<< HEAD
-=======
-                <FaAngleDown  className={style.z_10} />
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
+                <FaAngleDown className={style.z_10} />
               </div>
             </div>
           </div>
@@ -218,7 +191,7 @@ const Filter: React.FC = () => {
                     </div>
 
                     {/* Sub Category Section */}
-<<<<<<< HEAD
+
                     {selectedCategories.length > 0 && (
                       <div className={`${style.filterCategoryBox} ${style.filterSubCategoryBox}`}>
                         <div className={style.displa_flex}>
@@ -240,7 +213,7 @@ const Filter: React.FC = () => {
                               </div>
                             ))}
                           </div>
-                          
+
                           {/* Sub Sub Category Section */}
                           {selectedSubCategories.length > 0 && (
                             <div>
@@ -270,7 +243,7 @@ const Filter: React.FC = () => {
 
                     {selectedCategories.length > 0 && (
                       <div className={style.filter_btn_containe}>
-                        <button 
+                        <button
                           className={style.reset_btn}
                           onClick={resetFilters}
                         >
@@ -281,7 +254,6 @@ const Filter: React.FC = () => {
                         </button>
                       </div>
                     )}
-=======
                     <div
                       className={
                         filteredSubCategories.length > 0
@@ -344,25 +316,17 @@ const Filter: React.FC = () => {
                       </div>
                     </div>
 
-<<<<<<< HEAD
-                    {selectedCategories.length > 0 && (
-                      <div className={style.filter_btn_containe}>
-                        <button className={style.reset_btn}>Reset Filter</button>
-                        <button className={style.apply_btn}>Apply Filter</button>
-                      </div>
-                    )}
-=======
 
                     {selectedCategories.length > 0 ? <div className={style.filter_btn_containe}>
-  <button className={style.reset_btn}>Reset Filter</button>
-  <button className={style.apply_btn}>Apply Filter</button>
-</div> : ""
+                      <button className={style.reset_btn}>Reset Filter</button>
+                      <button className={style.apply_btn}>Apply Filter</button>
+                    </div> : ""
 
 
-}
+                    }
 
 
-{/* <div>
+                    {/* <div>
 
                       {selectedCategories.length > 0 ? "" :
 
@@ -371,8 +335,6 @@ const Filter: React.FC = () => {
                         </div>
                       }
                       </div> */}
->>>>>>> 2bf320d648f23f27a986222212b8fc64694c0716
->>>>>>> 73267724d945092babd7a2ed86c06831c4e93d83
                   </div>
                 </div>
               </div>
