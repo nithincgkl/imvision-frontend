7942aaa29b0d3b143d51bf23f3df5d6589cf23bf
Nithin <nithin@konceptslab.com>
2024-12-17 10:45:43 +0530

    17-12-2024

--- src/app/order-successful/page.tsx
+++ src/app/order-successful/page.tsx
@@ -1,144 +1,49 @@
-'use client';
+'use client'
 import React from 'react';
+import Link from 'next/link';
+import Wrapper from '@/layouts/wrapper';
+import FooterOne from '@/layouts/footers/FooterOne';
+import HeaderOne from '@/layouts/headers/HeaderOne';
 import style from "./style.module.css";
-import Wrapper from "@/layouts/wrapper";
-import FooterOne from "@/layouts/footers/FooterOne";
-import HeaderOne from "@/layouts/headers/HeaderOne";
-import Filter from "@/components/sale/filter";
-import ProductItem from "@/components/product-item/product-item";
-
-import rent_1 from "../../../public/assets/images/post/01.jpg";
-import rent_2 from "../../../public/assets/images/post/02.jpg";
-import rent_3 from "../../../public/assets/images/post/03.jpg";
-import rent_4 from "../../../public/assets/images/post/04.jpg";
-import rent_5 from "../../../public/assets/images/post/01.jpg";
-import rent_6 from "../../../public/assets/images/post/02.jpg";
-import rent_7 from "../../../public/assets/images/post/03.jpg";
-import rent_8 from "../../../public/assets/images/post/04.jpg";
-import rent_9 from "../../../public/assets/images/post/01.jpg";
-import rent_10 from "../../../public/assets/images/post/02.jpg";
-import rent_11 from "../../../public/assets/images/post/03.jpg";
-import rent_12 from "../../../public/assets/images/post/04.jpg";
 import LetsTalk from '@/components/home/lets-talk';
+import { FiDownload } from "react-icons/fi";
 
-const blog_data = [
-  {
-    id: 1,
-    img: rent_1,
-    title: `496×496 P1.9 Corner`,
-    des: `1274`,
-  },
-  {
-    id: 2,
-    img: rent_2,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 3,
-    img: rent_3,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 4,
-    img: rent_4,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 5,
-    img: rent_5,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 6,
-    img: rent_6,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 7,
-    img: rent_7,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 8,
-    img: rent_8,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 9,
-    img: rent_9,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 10,
-    img: rent_10,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 11,
-    img: rent_11,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  {
-    id: 12,
-    img: rent_12,
-    title: `ABSENnicon C Slim Series 110″`,
-    des: `15000`,
-  },
-  
-];
-
-const Page: React.FC = () => {
+// Main ContactPage Component
+const RentalConditions = () => {
   return (
-    <Wrapper>
+    <Wrapper> 
       <HeaderOne />
-
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            <div className={style["without-banner"]}>
-              {/* Common Top Section */}
-              <div className={style.topSection}>
+            <section className={style.contact_section}>
+              
+
+              <div className={style["order_successful"]}>
                 <div className="container-fluid">
                   <div className="row">
-                    <div className="col-12">
-                      <h1 className={style.pageTitle}>Rent Products</h1>
+                    <div className="col-md-12">
+                      <div className={style["order_successful_container"]}>
+                        <div>
+                        <img src="/assets/images/check.svg" className="w-100" alt="" />   
+                        <h5>Thank You for Shopping with Us!</h5> 
+                        <p>Your order ID is #453232. A detailed report has been sent to your registered email address. If you have any questions, feel free to contact our support team.</p>  
+                        <button  className="btn-one">Talk to Expert</button>  
+                        </div>              
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
 
-              <Filter />
+            </section>
 
-              <section className={style["product_section"]}>
-                <div className="container-fluid">
-                  <div className="row">
-                    {blog_data.map((item) => (
-                      <div className="col-xxl-3 col-xl-4 col-lg-6 col-md-6 col-sm-12" key={item.id}>
-                        <ProductItem item={item} />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </section>
-
-              <LetsTalk />
-            </div>
           </main>
-          <FooterOne />
+          {/* <FooterOne /> */}
         </div>
       </div>
     </Wrapper>
   );
 };
 
-export default Page;
+export default RentalConditions;
