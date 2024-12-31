cf57038cac454383684a2859c8205f32ef44b84a
kldeveloper <developer@konceptslab.com>
2024-12-31 11:38:58 +0530

    api integration

--- src/app/work-with-us/page.tsx
+++ src/app/work-with-us/page.tsx
@@ -21,101 +21,102 @@ const WorkWithUs: React.FC = () => {
   );
 };
 
-    interface Thumbnail {
+interface Thumbnail {
+  url: string;
+  formats: {
+    thumbnail: {
       url: string;
-      formats: {
-        thumbnail: {
-          url: string;
-        };
-      };
-    }
+    };
+  };
+}
 
 
 
-    interface CareerJob {
-      id: number;
-      documentId: string;
-      title: string;
-      location: string;
-      expert_in: string;
-      createdAt: string;
-      updatedAt: string;
-      publishedAt: string;
-      description: string | null;
-      slug: string;
-      thumbnail: Thumbnail | null;
-    }
+interface CareerJob {
+  id: number;
+  documentId: string;
+  title: string;
+  location: string;
+  expert_in: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  description: string | null;
+  slug: string;
+  thumbnail: Thumbnail | null;
+}
 
 
 const CareerBox = ({ job, onApply }: { job: CareerJob; onApply: (job: CareerJob) => void; }) => (
-        <div className={style["career_box"]}>
-          <div>
-            {job.thumbnail? (
-              <img src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${job.thumbnail.formats.thumbnail.url}`} className="w-100" alt={job.title} loading="lazy" />
-            ) : (
-              <div className={style["no-thumbnail"]}>No Image Available</div>
-            )}
-          </div>
- 
-
-
-        <div>
-            <h4>{job.title}</h4>
-            <p dangerouslySetInnerHTML={{ __html: job.description || "No description available." }} />            {/* <p><span>{`Posted on: ${new Date(job.publishedAt).toLocaleDateString()}`}</span></p> */}
-            <p><IoLocationOutline /> Location: {job.location}</p>
-            <p><PiSuitcaseSimpleLight /> Expert in: {job.expert_in}</p>
-            <div className={style["career_box_btn"]}>
-                <span>
-                    <a href="mailto:career@imvision.se" className="d-inline-flex align-items-center">
-                        Send mail to career@imvision.se <LuMoveUpRight />
-                    </a>
-                </span>
-                <span>
-                    <button className={style["apply_btn"]} onClick={() => onApply(job)}>Apply Now</button>
-                </span>
-            </div>
-        </div>
+  <div className={style["career_box"]}>
+    <div>
+      {job.thumbnail ? (
+        <img src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${job.thumbnail.formats.thumbnail.url}`} className="w-100" alt={job.title} loading="lazy" />
+      ) : (
+        <div className={style["no-thumbnail"]}>No Image Available</div>
+      )}
     </div>
-    );
+
+
+
+    <div>
+      <h4>{job.title}</h4>
+      <p dangerouslySetInnerHTML={{ __html: job.description || "No description available." }} />            {/* <p><span>{`Posted on: ${new Date(job.publishedAt).toLocaleDateString()}`}</span></p> */}
+      <p><IoLocationOutline /> Location: {job.location}</p>
+      <p><PiSuitcaseSimpleLight /> Expert in: {job.expert_in}</p>
+      <div className={style["career_box_btn"]}>
+        <span>
+          <a href="mailto:career@imvision.se" className="d-inline-flex align-items-center">
+            Send mail to career@imvision.se <LuMoveUpRight />
+          </a>
+        </span>
+        <span>
+          <button className={style["apply_btn"]} onClick={() => onApply(job)}>Apply Now</button>
+        </span>
+      </div>
+    </div>
+  </div>
+);
 
 const Career = () => {
-    const [careerOpenings, setCareerOpenings] = useState<CareerJob[]>([]);
-    const [displayedJobs, setDisplayedJobs] = useState(3);
-    const [selectedJob, setSelectedJob] = useState<CareerJob | null>(null);
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        resume: null as File | string | null,
-        message: "",
-        service: "",
-    });
-    const modalRef = useRef<HTMLDivElement>(null);
+  const [careerOpenings, setCareerOpenings] = useState<CareerJob[]>([]);
+  const [displayedJobs, setDisplayedJobs] = useState(3);
+  const [selectedJob, setSelectedJob] = useState<CareerJob | null>(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    resume: null as File | string | null,
+    message: "",
+    service: "",
+  });
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [isSubmitting, setIsSubmitting] = useState<Boolean>(false);
 
-    useEffect(() => {
-      const fetchJobs = async () => {
-          try {
-              const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}careers?populate=*`, {
-                  headers: {
-                      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-                  },
-              });          
-              
-  
-              // Accessing the jobs array from the response structure
-              const jobsArray = response.data.data; 
-  
-              // Ensure that the jobsArray is an array before setting state
-              if (Array.isArray(jobsArray)) {
-                  setCareerOpenings(jobsArray);
-              } else {
-                  console.error("Fetched data is not an array:", jobsArray);
-              }
-          } catch (error) {
-              console.error("Error fetching job data:", error);
-          }
-      };
-      fetchJobs();
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}careers?populate=*`, {
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+          },
+        });
+
+
+        // Accessing the jobs array from the response structure
+        const jobsArray = response.data.data;
+
+        // Ensure that the jobsArray is an array before setting state
+        if (Array.isArray(jobsArray)) {
+          setCareerOpenings(jobsArray);
+        } else {
+          console.error("Fetched data is not an array:", jobsArray);
+        }
+      } catch (error) {
+        console.error("Error fetching job data:", error);
+      }
+    };
+    fetchJobs();
   }, []);
 
   useEffect(() => {
@@ -152,11 +153,11 @@ const Career = () => {
     };
   }, [selectedJob]);
 
-const handleLoadMore = () => setDisplayedJobs((prev) => prev + 3);
-const handleApply = (job: CareerJob) => setSelectedJob(job);
-const handleCloseModal = () => setSelectedJob(null);
+  const handleLoadMore = () => setDisplayedJobs((prev) => prev + 3);
+  const handleApply = (job: CareerJob) => setSelectedJob(job);
+  const handleCloseModal = () => setSelectedJob(null);
 
-const handleInputChange = (
+  const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value, type } = e.target;
@@ -178,66 +179,75 @@ const handleInputChange = (
     }
   };
 
-const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-const file = e.target.files ? e.target.files[0] : null;  
-  setFormData((prevState) => ({
-    ...prevState,
-    resume: file,
-  }));
-};
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    setFormData((prevState) => ({
+      ...prevState,
+      resume: file,
+    }));
+  };
 
-const handleFormSubmit = async (e: React.FormEvent) => {
-  e.preventDefault();
-  if (!formData.resume) {
-    console.error("No file selected");
-    return;
-  } 
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!formData.resume) {
+      console.error("No file selected");
+      return;
+    }
 
-  
-const uploadData = new FormData();
-  uploadData.append("files", formData.resume as File, (formData.resume as File).name);
 
-  try {
-    const uploadResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}upload`, uploadData, {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-        'Content-Type': 'multipart/form-data'
-      }
-    });
-
-const uploadId = (uploadResponse.data[0].id);
-
-const careerFormData = {
-      data: {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        resume: uploadId,
-        job: { 
-          connect: [selectedJob ? selectedJob.documentId: null] 
-        },
-        comments:formData.message
-
-      }
-    };
-    
+    const uploadData = new FormData();
+    uploadData.append("files", formData.resume as File, (formData.resume as File).name);
 
     try {
-      const careerResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}job-enquiries?populate=job`, careerFormData, {
+      setIsSubmitting(true);
+      const uploadResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}upload`, uploadData, {
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-          'Content-Type': 'application/json'
+          'Content-Type': 'multipart/form-data'
         }
       });
 
-      handleCloseModal();
+      const uploadId = (uploadResponse.data[0].id);
+
+      const careerFormData = {
+        data: {
+          name: formData.name,
+          email: formData.email,
+          phone: formData.phone,
+          resume: uploadId,
+          job: {
+            connect: [selectedJob ? selectedJob.documentId : null]
+          },
+          comments: formData.message
+
+        }
+      };
+
+
+      try {
+        const careerResponse = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}job-enquiries?populate=job`, careerFormData, {
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+            'Content-Type': 'application/json'
+          }
+        });
+
+        handleCloseModal();
+      } catch (error) {
+        console.error(error);
+        setIsSubmitting(false);
+      }
+      finally {
+        setIsSubmitting(true);
+      }
     } catch (error) {
       console.error(error);
+      setIsSubmitting(false);
     }
-  } catch (error) {
-    console.error(error);
-  }
-};
+    finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <Wrapper>
@@ -249,19 +259,15 @@ const careerFormData = {
               <div className={style["contact_banner"]}>
                 <div className="container-fluid">
                   <div className="row">
-                    <div className="col-md-8">
+                    <div className="col-md-6">
                       <h1 className={style.pageTitle}>
                         Let Our Team <br />
                         Be Your New Team
                       </h1>
                     </div>
-                    <div className="col-md-4">
-                      <p>
-                        Glad you are interested in working with us at IM
-                        Vision! Below you see a couple of the positions we are
-                        currently looking for, we are always looking for new
-                        talent and even if the position you are looking for is
-                        not currently available, do not hesitate to contact us!
+                    <div className={`col-md-6 ${style.secondary_text_container}`}>
+                      <p className={style.secondary_header_text}>
+                        Glad you are interested in working with us at IM Vision!<br />Below you see a couple of the positions we are currently looking for, we are always looking for new talent and even if the position you are looking for is not currently available, do not hesitate to contact us!
                       </p>
                     </div>
                   </div>
@@ -288,7 +294,7 @@ const careerFormData = {
                     </div>
 
                     <div className="col-md-12 text-center">
-                      <button className={style["talk-btn"]}>
+                      <button className={style["talk-btn"]} onClick={() => window.location.href = '/contact'}>
                         Talk to Expert
                       </button>
                     </div>
@@ -379,7 +385,7 @@ const careerFormData = {
                             value={formData.service}
                             onChange={handleInputChange}
                           >
-                          <option value="">{selectedJob ? selectedJob.title : "Select Service"}</option>
+                            <option value="">{selectedJob ? selectedJob.title : "Select Service"}</option>
                           </select>
                           <IoChevronDown className={style.selectIcon} />
                         </div>
@@ -407,8 +413,8 @@ const careerFormData = {
                       </div>
                       <div className="row">
                         <div className="col-md-12 mb-3">
-                          <button type="submit" className={style.talk_btn}>
-                            Submit
+                          <button type="submit" className={style.talk_btn} onClick={handleFormSubmit}>
+                            {isSubmitting ? "Submitting..." : "Submit"}
                           </button>
                           <button
                             type="button"
