35eb65e80dc3563aadc0f5d0b38f963705f68060
kldeveloper <developer@konceptslab.com>
2025-01-01 16:58:25 +0530

    removed pdf size

--- src/app/rental-conditions/page.tsx
+++ src/app/rental-conditions/page.tsx
@@ -20,7 +20,7 @@ const RentalConditions: React.FC = () => {
 // Main ContactPage Component
 const Page = () => {
   return (
-    <Wrapper> 
+    <Wrapper>
       <HeaderOne />
       <div id="smooth-wrapper">
         <div id="smooth-content">
@@ -53,18 +53,18 @@ const Page = () => {
                           <li>The renter is responsible for damage to the rental equipment, theft, etc., and is liable for compensation up to the full value of the equipment as well as other costs incurred by IMPROD AB due to such damage, from the time the equipment leaves IMPROD AB's warehouse until it is returned. This also applies to cases where IMPROD AB arranged/ordered delivery/collection. To the extent that IMPROD AB's own insurance, according to IMPROD AB's assessment, may apply, the lessee is liable for compensation for all costs incurred by IMPROD AB due to such damage, including deductibles agreed in IMPROD AB's insurance.</li>
                           <li>IMPROD AB does not compensate for failed or canceled performances, recordings or the like caused by equipment faults or other circumstances beyond IMPROD AB's control.</li>
                           <li>Cancellation of booked equipment and staff must be done no later than two weeks before the start of the rental period. If cancellation occurs later, 50% of the rental amount will be charged. If cancellation is made the day before or on the same day as the start of the rental period, 100% of the rental amount will be charged. For equipment booked but not collected, the agreed rental cost is charged unless otherwise agreed.</li>
-                          <li>If the lessee does not take into account the above rental conditions, delays the agreed payment, goes bankrupt, ends up insolvent, is declared incompetent, dies or in some other way breaks the rental agreement entered into, IMPROD AB has the right to immediately repossess the equipment and cancel the agreement. In that case, the lessee must pay the rental cost until the day IMPROD AB takes the equipment back.</li>                        
+                          <li>If the lessee does not take into account the above rental conditions, delays the agreed payment, goes bankrupt, ends up insolvent, is declared incompetent, dies or in some other way breaks the rental agreement entered into, IMPROD AB has the right to immediately repossess the equipment and cancel the agreement. In that case, the lessee must pay the rental cost until the day IMPROD AB takes the equipment back.</li>
                         </ol>
-                        <div  className={style["download_btn_container"]}>
+                        <div className={style["download_btn_container"]}>
                           <div><img src="/assets/images/download.jpg" className="w-100" alt="" /></div>
-                          <a 
-                            href="/lease-agreement.pdf" 
-                            download 
+                          <a
+                            href="/lease-agreement.pdf"
+                            download
                             className={style["download-link"]}
                           >
                             <h4>Download Lease Agreement</h4>
                             <div className={style["download_btn"]}>
-                              <span>PDF  1.5 MB</span>
+                              <span>PDF</span>
                               <span><FiDownload /></span>
                             </div>
                           </a>
